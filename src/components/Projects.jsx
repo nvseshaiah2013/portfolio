3c84c6a54277aa8c7dc218056d27c76ba973459f
seshu <nvseshaiah2013@gmail.com>
2022-02-23 23:25:36 +0530

    Project urls updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
                             <img src="notebook.gif" alt="Covid Icon" className="h-20 w-20 mx-auto" />
                             <h2 className="leading-loose font-bold text-xl text-blue-900"> Hisaab Kitaab</h2>
                             <p className="leading-relaxed mb-3 mx-1">It is a tool to track your expenses on your friends while on trips, or you track the items you have lended to them.</p>
-                            <a href="https://nvseshaiah2013.github.io/covid-dashboard/" className="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            <a href="https://github.com/nvseshaiah2013/hisaab_react/" className="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 target={'_blank'} rel="noreferrer"
                             > See Project <FontAwesomeIcon icon={faArrowRight} /></a>
                         </div>
@@ -36,7 +36,7 @@ const Projects = () => {
                             <img src="confetti.png" alt="Covid Icon" className="h-20 w-20 mx-auto" />
                             <h2 className="leading-loose font-bold text-xl text-blue-900"> Colors - Event Manager</h2>
                             <p className="leading-relaxed mb-3 mx-1">A one stop shop for your event management needs, from organizing and onboarding participants, to distributing certificates. </p>
-                            <a href="https://nvseshaiah2013.github.io/covid-dashboard/" className="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            <a href="https://github.com/nvseshaiah2013/Capstone-Project/" className="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 target={'_blank'} rel="noreferrer"
                             > See Project <FontAwesomeIcon icon={faArrowRight} /></a>
                         </div>
